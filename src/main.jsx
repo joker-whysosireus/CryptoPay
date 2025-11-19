4359b0b65c458b8bb5b89c98136d318297498032
joker-whysosireus <7kp2x@atomicmail.io>
2025-11-19 11:54:35 +0300

    ton

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { TonConnectUIProvider } from '@tonconnect/ui-react'
 import App from './App'
 
 // TON манифест - замените на ваш реальный URL
-const manifestUrl = 'https://gist.githubusercontent.com/your-username/your-manifest-id/raw/tonconnect-manifest.json'
+const manifestUrl = 'https://raw.githubusercontent.com/joker-whysosireus/manifest-info-watch-earn/main/manifest.txt'
 
 const root = createRoot(document.getElementById('root'))
 root.render(
